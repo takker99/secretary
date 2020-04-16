ab4ef53d7673f37db07e2664e3d7a6e8aec38f1b
takker99 <electronic.homuhomu@gmail.com>
2020-04-16 20:45:03 +0900

    :sparkles: add attributes

--- src/model.py
+++ src/model.py
@@ -11,7 +11,10 @@ Todo:
     * conf.pyの``todo_include_todos = True``にしないと表示されない
 
 """
-
+import datetime
+from typing import List
+from enum import Enum, auto
+import uuid
 # TODO: 作成するクラス一覧
 #
 # - tasks: taskの追加・変更・複製・削除を行う
@@ -22,20 +25,61 @@ Todo:
 # tagによる検索機能もつける
 
 
+class Status(Enum):
+    """
+    Taskに指定できるstatusを定義した列挙型
+    """
+    ACTIVE = auto()    # 有効なタスク
+    ARCHIVED = auto()  # 期限切れもしくは完了
+
+
 class Task:
-    pass
+    def __init__(self):
+        self.__id = uuid.uuid4()
+        self.__summary = ''                          # type: str
+        self.__description = ''                      # type: str
+        self.__length = 0                            # type: int
+        self.__deadline = datetime.datetime()        # type: datetime.datetime
+        self.__is_completed = 0.0                    # type: float
+        self.__status = Status.ACTIVE                # type: Status
+        self.__priority = 0                          # type: int
+        self.__tag_list = list()                     # type: List[str]
+        self.__location = ''                         # type: str
+        self.__created_at = datetime.datetime.now()  # type: datetime.datetime
+        self.__updated_at = datetime.datetime.now()  # type: datetime.datetime
 
 
 class Record:
-    pass
+    def __init__(self):
+        self.__id = uuid.uuid4()
+        self.__summary = ''                          # type: str
+        self.__is_completed = 0.0                    # type: float
+        self.__begin = datetime.datetime.now()       # type: datetime.datetime
+        self.__end = datetime.datetime.now()         # type: datetime.datetime
+        self.__tag_list = list()                     # type: List[str]
+        self.__location = ''                         # type: str
+        self.__commit_message = ''                     # type: str
 
 
 class Project:
-    pass
+    def __init__(self):
+        self.__id = uuid.uuid4()
+        self.__name = 'no-name'                      # type: str
+        self.__reference = ['../memo/memo.md']         # type: List[str]
+        self.__begin = datetime.datetime.now()       # type: datetime.datetime
+        self.__end = datetime.datetime.now()         # type: datetime.datetime
+        self.__status = Status.ACTIVE                # type: Status
+        self.__priority = 0                          # type: int
+        self.__tag_list = list()                     # type: List[str]
+        self.__task_list = list()                    # type: List[str]
+        self.__created_at = datetime.datetime.now()  # type: datetime.datetime
+        self.__updated_at = datetime.datetime.now()  # type: datetime.datetime
 
 
 class Tag:
-    pass
+    def __init__(self):
+        self.__id = uuid.uuid4()
+        self.__name = 'no-name'                      # type: str
 
 
 class ProjectManager:
