acc20ca0920d7e2ac1ec1230ac22ee5a35729296
takker99 <electronic.homuhomu@gmail.com>
2020-04-17 16:35:02 +0900

    :sparkles: prepare for the core function of this project
    ファイルといくらかのヘッダー情報を用意しただけ

---
+++ src/core.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""モジュールの説明タイトル
+
+    - secretary操作の窓口を担うクラス
+    - 変換formatは./README.mdを参照
+
+Todo:
+
+"""
+import datetime
+from typing import List, Dict
+from enum import Enum, auto
+import uuid
+# TODO: 作成するクラス一覧
+#
