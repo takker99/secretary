c38b2db8f1d34cedc8f0bb4d29bcfcbfa18195bb
takker99 <electronic.homuhomu@gmail.com>
2020-04-21 18:53:40 +0900

    :tada: :alembic: make the WPF client of Task management system

---
+++ practice/HelloDataBaseWPF/ViewModels/MainWindow.cs
@@ -0,0 +1,14 @@
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace HelloDataBaseWPF.ViewModels
+{
+    public class MainWindow : BindableBase
+    {
+        public MainWindow()
+        {
+
+        }
+    }
+}
